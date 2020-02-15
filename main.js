de6970b1b18e52441dda0241aebfe4cc939b827c
ergou <15066090444@qq.com>
2020-02-16 01:15:56 +0800

    页面布局及js部分已完成，接下来继续优化ui界面

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-//改变鼠标样式，后期须针对不同的状态进行优化
-document.body.style.cursor = "crosshair";
 
+//选择器
 const canvas = document.getElementById("canvas");
 const empty = document.getElementsByClassName("empty")[0];
 const tools = document.getElementsByClassName("tools")[0];
@@ -8,115 +7,213 @@ const download = document.getElementsByClassName("download")[0];
 const eraser = document.getElementsByClassName('eraser')[0]
 const pen = document.getElementsByClassName('pen')[0]
 
-
-//橡皮擦功能
-eraser.onclick = function(){
-  canvas.classList.add('eraser-cursor')
-  ctx.strokeStyle = 'white'
-}
-
-
-//调节画笔颜色
-const red = document.getElementsByClassName('red')[0]
-const green = document.getElementsByClassName('green')[0]
+//画笔
 const black = document.getElementsByClassName('black')[0]
+const red = document.getElementsByClassName('red')[0]
+const orange = document.getElementsByClassName('orange')[0]
+const green = document.getElementsByClassName('green')[0]
+const blue = document.getElementsByClassName('blue')[0]
+const violet = document.getElementsByClassName('violet')[0]
 
-
+//初始化画布
 canvas.width = document.documentElement.clientWidth;
 canvas.height = document.documentElement.clientHeight;
-
 let ctx = canvas.getContext("2d");
 
-startPainting();
+var eraserEnabled = false;
 
-function startPainting() {
-  let painting = false;
-  let lastPoint = {
-    x: undefined,
-    y: undefined
-  };
+var lineWidth = 5
 
-  let newPoint = {
-    x: undefined,
-    y: undefined
-  };
+class Canvas {
 
-  canvas.onmousedown = function(e) {
-    painting = true;
+  constructor() {
+    this.initCanvas()
+    this.initBrush()
+    this.bindEvents()
+  }
 
-    lastPoint.x = e.clientX;
-    lastPoint.y = e.clientY;
-  };
+  //屏幕宽度变化时及时调节画布的大小
+  initCanvas(){
+    canvasSize()
 
-  canvas.onmousemove = function(e) {
-    if (painting) {
-      newPoint.x = e.clientX;
-      newPoint.y = e.clientY;
+    window.onresize = function () {
+      canvasSize()
+    }
 
-      ctx.beginPath();
-      ctx.moveTo(lastPoint.x, lastPoint.y);
-      ctx.lineTo(newPoint.x, newPoint.y);
-      ctx.closePath();
-      ctx.stroke();
+    function canvasSize() {
 
-      if (lastPoint.x) {
-        lastPoint.x = newPoint.x;
-        lastPoint.y = newPoint.y;
+      var pageWidth = document.documentElement.clientWidth
+      var pageHeight = document.documentElement.clientHeight
+      canvas.width =  pageWidth
+      canvas.height = pageHeight
+    }
+
+  }
+
+  //初始化笔刷
+  initBrush() {
+    let painting = false;
+    let lastPoint = {
+      x: undefined,
+      y: undefined
+    };
+
+    let newPoint = {
+      x: undefined,
+      y: undefined
+    };
+
+    //设备检测
+    if(document.body.ontouchstart === undefined){
+      //如果是非触屏设备
+
+
+      canvas.onmousedown = function (e) {
+        painting = true;
+
+        lastPoint.x = e.clientX;
+        lastPoint.y = e.clientY;
+      };
+
+      canvas.onmousemove = function (e) {
+        if (painting) {
+          newPoint.x = e.clientX;
+          newPoint.y = e.clientY;
+
+          drawCricle(newPoint.x,newPoint.y,lineWidth/2)
+          drawLine(lastPoint.x,lastPoint.y,newPoint.x,newPoint.y)
+
+          lastPoint.x = newPoint.x
+          lastPoint.y = newPoint.y
+        }
+      };
+
+      canvas.onmouseup = function () {
+        painting = false;
+      };
+    }else{
+      //如果是触屏设备
+      canvas.ontouchstart = function (e) {
+        painting = true
+
+        lastPoint.x = e.touches[0].clientX
+        lastPoint.y = e.touches[0].clientY
+      }
+
+      canvas.ontouchmove = function (e) {
+        if(painting){
+          newPoint.x = e.touches[0].clientX
+          newPoint.y = e.touches[0].clientY
+
+          drawCricle(newPoint.x,newPoint.y,lineWidth/2)
+          drawLine(lastPoint.x,lastPoint.y,newPoint.x,newPoint.y)
+
+          lastPoint.x = newPoint.x
+          lastPoint.y = newPoint.y
+        }
+      }
+
+      canvas.ontouchend = function (e) {
+        painting = false
+
       }
     }
-  };
 
-  canvas.onmouseup = function() {
-    painting = false;
-  };
+    function drawLine(x1, y1, x2, y2) {
+      ctx.beginPath();
+      ctx.moveTo(x1, y1)
+      ctx.lineWidth = lineWidth;
+      ctx.lineTo(x2, y2)
+      ctx.stroke();
+      ctx.closePath();
+    }
+
+    function drawCricle(x, y, radius) {
+      ctx.beginPath();
+      ctx.arc(x, y, radius, 0, Math.PI * 2);
+      ctx.fill();
+    }
+  }
+
+  bindEvents() {
+    //橡皮擦功能
+    eraser.onclick = function () {
+      eraserEnabled = true
+      canvas.classList.add('eraser-cursor')
+      ctx.lineWidth = 100
+      ctx.strokeStyle = 'white'
+    }
+
+    // 清空画面
+    empty.onclick = function () {
+      ctx.clearRect(0,0,canvas.width,canvas.height)
+    };
+
+    //下载画布，这个地方需要我下来好好研究一下
+    download.onclick = function () {
+      var compositeOperation = ctx.globalCompositeOperation;
+      ctx.globalCompositeOperation = "destination-over";
+      ctx.fillStyle = "#fff";
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+      var imageData = canvas.toDataURL("image/png");
+      ctx.putImageData(
+          ctx.getImageData(0, 0, canvas.width, canvas.height),
+          0,
+          0
+      );
+      ctx.globalCompositeOperation = compositeOperation;
+      var a = document.createElement("a");
+      document.body.appendChild(a);
+      a.href = imageData;
+      a.download = "mypaint";
+      a.target = "_blank";
+      a.click();
+    };
+
+    black.onclick = function () {
+      eraserEnabled = false
+      canvas.classList.remove('eraser-cursor')
+      ctx.fillStyle = 'black'
+      ctx.strokeStyle = 'black'
+    }
+
+    red.onclick = function () {
+      eraserEnabled = false
+      canvas.classList.remove('eraser-cursor')
+      ctx.fillStyle = 'red'
+      ctx.strokeStyle = 'red'
+    }
+
+    orange.onclick = function () {
+      eraserEnabled = false
+      canvas.classList.remove('eraser-cursor')
+      ctx.fillStyle = 'orange'
+      ctx.strokeStyle = 'orange'
+    }
+
+    green.onclick = function () {
+      eraserEnabled = false
+      canvas.classList.remove('eraser-cursor')
+      ctx.fillStyle = 'green'
+      ctx.strokeStyle = 'green'
+    }
+
+    blue.onclick = function () {
+      eraserEnabled = false
+      canvas.classList.remove('eraser-cursor')
+      ctx.fillStyle = 'blue'
+      ctx.strokeStyle = 'blue'
+    }
+
+    violet.onclick = function () {
+      eraserEnabled = false
+      canvas.classList.remove('eraser-cursor')
+      ctx.fillStyle = 'violet'
+      ctx.strokeStyle = 'violet'
+    }
+
+  }
 }
 
-// 清空画面
-empty.onclick = function() {
-  ctx.fillStyle = "#fff";
-  ctx.beginPath();
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  ctx.closePath();
-};
 
-tools.onmouseover = function() {
-  document.body.style.cursor = "default";
-};
-
-tools.onmouseout = function() {
-  document.body.style.cursor = "crosshair";
-};
-
-//下载画布
-download.onclick = function() {
-  var compositeOperation = ctx.globalCompositeOperation;
-  ctx.globalCompositeOperation = "destination-over";
-  ctx.fillStyle = "#fff";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  var imageData = canvas.toDataURL("image/png");
-  ctx.putImageData(
-    ctx.getImageData(0, 0, canvas.width, canvas.height),
-    0,
-    0
-  );
-  ctx.globalCompositeOperation = compositeOperation;
-  var a = document.createElement("a");
-  document.body.appendChild(a);
-  a.href = imageData;
-  a.download = "mypaint";
-  a.target = "_blank";
-  a.click();
-};
-
-red.onclick = function(){
-    ctx.strokeStyle = 'red'
-}
-
-green.onclick = function(){
-    ctx.strokeStyle = 'green'
-}
-
-black.onclick = function(){
-    ctx.strokeStyle = 'black'
-}
-
+var tabs = new Canvas()
